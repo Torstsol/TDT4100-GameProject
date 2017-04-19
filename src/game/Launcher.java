c4d4d07a7ea991ed2f4b7698f837045cd201b378
SosaPrinsen <andreas@arnholm.org>
2017-04-19 14:17:25 +0200

    New Title

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		
-		Game game = new Game("WolfRunner Alpha", 1280, 720);
+		Game game = new Game("AciDance 2017", 1280, 720);
 		game.start();
 		
 	}
